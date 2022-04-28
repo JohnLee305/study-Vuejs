61ae9c23acd81cd2c7536a9b1bc4627135c6bacf
JohnLee305 <johnathanl305@gmail.com>
2022-04-28 19:06:11 +0900

    2022-04-28 검색결과 구현 완료

--- 2-vue/js/app.js
+++ 2-vue/js/app.js
@@ -1,3 +1,4 @@
+import SearchModel from "./models/SearchModel.js";
 new Vue({
     // el : 화면 컴포넌드 아이디를 입력
     el: '#app',
@@ -6,11 +7,13 @@ new Vue({
     data: {
         // msg: 'hello world'
         // query : 입력을 담당하는 데이터 할당.
-        query : ''
+          query : ''
+        , searchResult : []
+        , submitted : false
     }
     , methods : {
          onSubmit(e){
-            
+            this.search()
         }
         , onReset(e){
             // 데이터 안에 있는 변수 접근할때는 그냥 this.변수명으로 접근이 가능하다.
@@ -19,6 +22,12 @@ new Vue({
         , onKeyup(e){
             // 내부에 있는 메서드에 접근할때도 그냥 this로 접근이 가능하다.
             if(!this.query.length) this.onReset()
+        }
+        , search(){
+            SearchModel.list().then(data => {
+                this.submitted = true
+                this.searchResult = data 
+            })
         }
     }
 })
